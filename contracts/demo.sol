03dcfff7d3a4f2ed027dfc4f1ee3211e49bcbd88
gpablo3 <giancarlo@api3.org>
2022-10-25 10:29:51 -0600

    Add boilerplate for demo contract

--- contracts/demo.sol
+++ contracts/demo.sol
@@ -3,38 +3,75 @@
 pragma solidity ^0.8.15;
 
 import "@api3/airnode-protocol/contracts/rrp/requesters/RrpRequesterV0.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Demo is RrpRequesterV0 {
+contract Demo is RrpRequesterV0, ERC20, Ownable {
 
     mapping(bytes32 => bool) public incomingFulfillments;
-    mapping(bytes32 => int256) public fulfilledData;
+    mapping(bytes32 => bytes32) public fulfilledData;
+
+    address public airnode;
+    mapping(uint8 => bytes32) public endpointIdsUints256;
+    address private sponsorWallet;
 
     event FulfilledRequest (bytes32 requestId);
+    event SetRequestParameters(
+        address airnodeAddress,
+        address sponsorAddress
+    );
 
-    constructor (address airnodeRrpAddress) public RrpRequesterV0(airnodeRrpAddress) {}
+    constructor (
+        address _airnodeRrpAddress,
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        uint8 _decimals
+        )
+    RrpRequesterV0 (
+        _airnodeRrpAddress
+    )
+    ERC20 (
+        _tokenName,
+        _tokenSymbol,
+        _decimals
+    )
+        {}
 
-    function callTheAirnode(
+    function setRequestParameters(
+        address _airnode,
+        bytes32 _endpointIdUint256,
+        address _sponsorWallet
+    ) external onlyOwner {
+        airnode = _airnode;
+        sponsorWallet = _sponsorWallet;
+        emit SetRequestParameters(
+            airnode, 
+            sponsorWallet
+        );
+    }
+
+    function callTheAirnode (
         address airnode,
-        bytes32 endpointId,
+        uint8 endpointId,
         address sponsor,
         address sponsorWallet,
-        bytes calldata parameters // Inbound API parameters which may already be ABI encoded
+        bytes calldata parameters
         ) external {
 
-            bytes32 requestId = airnodeRrp.makeFullRequest( // Make the Airnode request
-                airnode,                        // airnode
-                endpointId,                     // endpointId
-                sponsor,                        // sponsor's address
-                sponsorWallet,                  // sponsorWallet
-                address(this),                  // fulfillAddress
-                this.airnodeCallback.selector,  // fulfillFunctionId
-                parameters                      // API parameters
+            bytes32 requestId = airnodeRrp.makeFullRequest (
+                airnode,                            // airnode
+                endpointIdsUints256[endpointId],    // endpointId
+                sponsor,                            // sponsor's address
+                sponsorWallet,                      // sponsorWallet
+                address(this),                      // fulfillAddress
+                this.airnodeCallback.selector,      // fulfillFunctionId
+                parameters                          // API parameters
             );
 
             incomingFulfillments[requestId] = true;
     }
 
-  function airnodeCallback (   // The AirnodeRrpV0.sol protocol contract will callback here.
+    function airnodeCallback (
         bytes32 requestId,
         bytes calldata data 
         ) external onlyAirnodeRrp {
