4fa4ff10b6aeda8d8789752ba8567d7b30bdc7f5
gpablo3 <giancarlo@api3.org>
2022-11-05 11:50:14 -0600

    [WIP] Add wallet endpoint logic on-chain

--- contracts/demo.sol
+++ contracts/demo.sol
@@ -8,28 +8,59 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Demo is RrpRequesterV0, ERC20, Ownable {
 
-    mapping(bytes32 => bool) public incomingFulfillments;
-    mapping(bytes32 => bytes32) public fulfilledData;
+    address public airnode;
+    address private sponsorAddress;
+    address private sponsorWallet;
 
     struct Endpoint {
         bytes32 endpointId;
         bytes4 functionSelector;
     }
 
-    address public airnode;
-    mapping(uint8 => Endpoint) public endpointsIds;
-    address private sponsorWallet;
+    struct Operation {
+        address senderAddress,
+        address recieverAdress,
+        bytes payload
+    }
 
-    event FulfilledRequest (bytes32 indexed requestId);
-    event SetRequestParameters(
-        address airnodeAddress,
-        address sponsorAddress
+    struct TokenEscrow {
+        bool liquidMint;
+        bool evmConfirmation;
+        bytes32 requestId;
+        address sender;
+        address reciever;
+        uint256 amount;
+    }
+
+    mapping(uint8 => Endpoint) public endpointsIds;
+    mapping(bytes32 => bool) public incomingFulfillments;
+    mapping(bytes32 => Operation) private requestToOperation;
+    mapping(uint256 => TokenEscrow) public transactionHistory;
+    mapping(address => TokenEscrow[]) public currentTransactions;
+
+    event SuccessfulGet (
+        bytes32 indexed requestId, 
+        bytes apiResult
     );
-    event SetEndpoint(
+    event SetRequestParameters (
+        address airnodeAddress,
+        address sponsorAdress,
+        address sponsorWallet
+    );
+    event SetEndpoint (
         uint8 _index,
         bytes32 _newEndpointId,
         bytes4 _newEndpointSelector
     );
+    event WalletOperation (
+        address indexed senderAddress,
+        bytes payload
+    );
+    event TxOperation (
+        address indexed senderAddress,
+        address indexed recieverAddress,
+        bytes payload
+    )
 
     constructor (
         address _airnodeRrpAddress,
@@ -49,23 +80,23 @@ contract Demo is RrpRequesterV0, ERC20, Ownable {
 
     function setRequestParameters (
         address _airnode,
+        address _sponsorAddress,
         address _sponsorWallet
     ) external onlyOwner {
         airnode = _airnode;
+        sponsorAddress = _sponsorAddress;
         sponsorWallet = _sponsorWallet;
         emit SetRequestParameters(
-            airnode, 
-            sponsorWallet
+            _airnode,
+            _sponsorAddress,
+            _sponsorWallet
         );
     }
 
-    /**
-     * @dev Pending to add `Update Endpoint Logic`
-     */
-    function setEndpointLogic (
+    function _setEndpointReference (
         bytes32 _endpointId,
         bytes4 _functionSelector
-    ) external onlyOwner {
+    ) private {
         Endpoint memory newEndpoint;
 
         newEndpoint.endpointId = _endpointId;
@@ -79,19 +110,17 @@ contract Demo is RrpRequesterV0, ERC20, Ownable {
             _functionSelector);
     }
 
-    function callTheAirnode (
-        address airnode,
-        uint8 endpointId,
-        address sponsor,
-        address sponsorWallet,
+    function operationsWallet (
+        uint8 endpointIndex,
+        address _senderAddress,
         bytes calldata parameters
         ) external onlyOwner {
-            Endpoint memory functionData = endpointsIds[endpointId];
+            Endpoint memory functionData = endpointsIds[endpointIndex];
 
             bytes32 requestId = airnodeRrp.makeFullRequest (
                 airnode,                            // airnode
                 functionData.endpointId,            // endpointId
-                sponsor,                            // sponsor's address
+                sponsorAddress,                     // sponsor's address
                 sponsorWallet,                      // sponsorWallet
                 address(this),                      // fulfillAddress
                 functionData.functionSelector,      // fulfillFunctionId
@@ -99,17 +128,78 @@ contract Demo is RrpRequesterV0, ERC20, Ownable {
             );
 
             incomingFulfillments[requestId] = true;
+            requestToOperation[requestId] = Operation (
+                _senderAddress, 
+                address(0),
+                parameters);
+            emit WalletOperation(_senderAddress, parameters);
     }
 
-    function airnodeCallback (
+    function operationsTx (
+        uint8 endpointIndex,
+        address senderAddress,
+        address recieverAddress,
+        bytes calldata parameters
+    ) external onlyOwner {
+        Endpoint memory functionData = endpointsIds[endpointIndex];
+
+        bytes32 requestId = airnodeRrp.makeFullRequest (
+            airnode,                            // airnode
+            functionData.endpointId,            // endpointId
+            sponsorAddress,                     // sponsor's address
+            sponsorWallet,                      // sponsorWallet
+            address(this),                      // fulfillAddress
+            functionData.functionSelector,      // fulfillFunctionId
+            parameters                          // API parameters
+        );
+
+        incomingFulfillments[requestId] = true;
+        requestToOperation[requestId] = Operation (
+            senderAddress, 
+            address(0),
+            parameters);
+        emit TxOperation()
+    }
+
+    function walletGet (
+        bytes32 requestId,
+        bytes calldata data
+    ) external onlyAirnodeRrp {
+        require(incomingFulfillments[requestId], "No such request made");
+
+        string memory decodedData = abi.decode(data, (string));
+        delete incomingFulfillments[requestId];
+
+        emit SuccessfulGet(requestId, decodedData);
+
+    }
+
+    function walletLabelGet (
+        bytes32 requestId,
+        bytes calldata data
+    ) external onlyAirnodeRrp {
+        require(incomingFulfillments[requestId], "No such request made");
+
+        string memory decodedData = abi.decode(data, (string));
+        delete incomingFulfillments[requestId];
+
+        emit SuccessfulGet(requestId, decodedData);
+    }
+
+    function txSend (
         bytes32 requestId,
         bytes calldata data 
         ) external onlyAirnodeRrp {
 
             require(incomingFulfillments[requestId], "No such request made");
-            delete incomingFulfillments[requestId];
-            int256 decodedData = abi.decode(data, (int256));
+            
+            string memory decodedData = abi.decode(data, (string));
             fulfilledData[requestId] = decodedData;
+            TokenEscrow memory currentToken = TokenEscrow(
+
+            ); 
+
+            delete incomingFulfillments[requestId];
             emit FulfilledRequest(requestId);
     }
 }
