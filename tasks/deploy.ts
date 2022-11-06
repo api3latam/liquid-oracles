bd0e3d3ec9c1a12129496b779b38352111cc268c
gpablo3 <giancarlo@api3.org>
2022-11-06 15:10:25 -0600

    Add deploy tasks for Demo contract

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,17 +1,36 @@
 import { providers, Wallet } from "ethers";
+import { AirnodeRrpAddresses } from '@api3/airnode-protocol';
 import { task } from "hardhat/config";
 
-import { getEnvVars } from "../utils"
+import { getEnvVars, writeJsonFile } from "../utils"
 
 task("deploy", "Deploy Compile Contracts for Repository")
     .setAction(async (_, hre) => {
-            const provider = new providers.AlchemyProvider("rinkeby", getEnvVars('ALCHEMY_KEY')[0])
+            const file = `addresses/demo${hre.network.name}.json`;
+            const provider = new providers.JsonRpcProvider (
+                getEnvVars (
+                    `${(hre.network.name).toUpperCase()}_RPC`)
+                    [0]
+                );
             const wallet = new Wallet(getEnvVars("PRIVATE_KEY")[0], provider);
+
             const factory = await hre.ethers.getContractFactory("Demo", wallet);
 
             try {
-                const contract = await factory.deploy("0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd");
+                const rrpAddress = AirnodeRrpAddresses[hre.network.config.chainId as number]; 
+                const contract = await factory.deploy(
+                    rrpAddress,
+                    "API3 wLIQUID",
+                    "APILQ",
+                    8
+                );
                 console.log (`Contract deployed to: ${contract.address}`);
+
+                console.log('Saving addresses to file...\n')
+                writeJsonFile({
+                    path: `/${file}`,
+                    data: { demo : contract.address }
+                });
             } catch (e) {
                 console.error(e);
             }
